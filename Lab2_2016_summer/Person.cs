7980d67a1b23c9dd817f10cff9d063579b1c20b2
Shivya B <sbalac17@my.centennialcollege.ca>
2016-05-25 15:28:41 -0400

    Lab 2

---
+++ Lab2_2016_summer/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2016_summer
+{
+    /**
+     * This class describes a person
+     * 
+     * @class Person
+     * */
+    class Person
+    {
+        // instance variables (a class is anything.. )
+        private string _name;
+        private int _age;
+        // this is a property (below)
+        public string Name // able to view things that are private (above)  
+        {
+            get { return _name; }
+        }
+
+        /**
+         * the constructor for a person
+         * 
+         * @method Person
+         * @param {string} name
+         * @param {int} age
+         * */
+        public Person(string name, int age) // this is a constructor to make a new person (so for new on the program page
+            //this part will be called. 
+        {
+            _name = name;
+            _age = age;
+        }
+        /**
+         * this displays a hello message from the person
+         * 
+         * @method SayHello
+         * */
+        public void SayHello()
+        {
+            Console.WriteLine("{0} says hello", _name);
+        }
+        /**
+         * this displays name and age of the person
+         * 
+         * @method ShowAge
+         * */
+        public void ShowAge()
+        {
+            Console.WriteLine("{0} is {1} years old", _name, _age);
+        }
+
+    }
+}
---
+++ Lab2_2016_summer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2016_summer
+{
+    /**
+     * This class is the "driver" class for our program
+     * 
+     * @class Program
+     * */
+    public class Program
+    {
+
+        /**
+         * the main method for out driver class
+         * 
+         * @method Main
+         * @param {string[]} args
+         * */
+        public static void Main(string[] args)
+        {
+           
+
+            Person shav = new Person("shav", 24);
+            Person Rohan = new Person("Rohan", 21);
+            //say hello and show age 
+            shav.ShowAge();
+            Rohan.ShowAge();
+            shav.SayHello();
+            Rohan.ShowAge();
+        }
+    }
+}
